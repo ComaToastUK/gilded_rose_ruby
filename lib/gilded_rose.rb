3d8f91ecdf785d4514808f8223e3740154528b2c
ComaToastUK <jesusmanyouatethedog@gmail.com>
2017-10-31 16:32:03 +0000

    refactored into seperate method for each item type

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,58 +1,53 @@
-require_relative 'item'
-
 class GildedRose
+
+  MINIMUM_QUALITY = 0
 
   def initialize(items)
     @items = items
+    @exceptions = ["Aged Brie", "Sulfuras, Hand of Ragnaros", "Backstage passes to a TAFKAL80ETC concert"]
   end
 
-  def update_quality()
+  def normal_item(item)
+    item.sell_in -= 1
+    return if item.quality == 0
+    item.quality -= 1
+    item.quality -= 1 if item.sell_in <= 0
+  end
+
+  def update_quality
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality -= 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality += 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality += 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
+      if @exceptions.include? item == false
+        normal_item(item)
+      elsif item.name == 'Aged Brie'
+        brie(item)
+      elsif item.name == "Sulfuras, Hand of Ragnaros"
+        sulfuras(item)
+      elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
+        backstage(item)
       end
     end
   end
+
+  def brie(item)
+    item.sell_in -= 1
+    return if item.quality >= 50
+    item.quality += 1
+    item.quality += 1 if item.sell_in <= 0
+  end
+
+  def sulfuras(item)
+    return
+  end
+
+  def backstage(item)
+    item.sell_in -= 1
+    return if item.quality >= 50
+    return item.quality = 0 if item.sell_in < 0
+    item.quality += 1
+    item.quality += 1 if item.sell_in < 10
+    item.quality += 1 if item.sell_in < 5
+  end
+
 end
 
 class Item
