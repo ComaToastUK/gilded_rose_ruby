786b895359a00589566548ef45f69ac4cf3e556a
ComaToastUK <jesusmanyouatethedog@gmail.com>
2017-10-31 12:45:26 +0000

    additional tests for indivisual item types

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -60,4 +60,25 @@ describe GildedRose do
     expect(items[0].quality).to be < 50
   end
 
+  it 'does not degrade if it is Sulfuras' do
+    items = [Item.new("Sulfuras, Hand of Ragnaros", 10, 50)]
+    GildedRose.new(items).update_quality()
+    expect(items[0].quality).to eq(50)
+  end
+
+  it 'does not reduce sell_in if it is Sulfuras' do
+    items = [Item.new("Sulfuras, Hand of Ragnaros", 10, 50)]
+    GildedRose.new(items).update_quality()
+    expect(items[0].sell_in).to eq(10)
+  end
+
+  it 'gains in quality after sell_in if it is Aged Brie' do
+    items = [Item.new("Aged Brie", 2, 2)]
+    n = 20
+    n.times do
+      GildedRose.new(items).update_quality()
+    end
+    expect(items[0].quality).to eq(40)
+  end
+
 end
